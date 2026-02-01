1783c6cbf3ec35fa3e5605d9a8608d7b8b8efade
Steven Alvarado <alvaradosteven315@gmail.com>
2026-01-31 20:29:49 -0500

    chore: generate controller / service based structure

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,6 @@
-import { sValidator } from "@hono/standard-validator";
-import { eq, gt, and, desc } from "drizzle-orm";
 import { Hono } from "hono";
-import { createDb } from "./db/client";
-import { users, verificationOtps } from "./db/schema";
-import {
-  loginRequestOtpSchema,
-  loginVerifyOtpSchema,
-} from "./routeSchemas/route-schemas";
-import { RawResponse } from "./types";
-import { generateOtp } from "./utils";
-import { getJWTSession } from "./session";
-
-type Env = {
-  DATABASE_URL: string;
-  SECRET: string;
-};
+import { routes } from "./routes";
+import { Env } from "./types";
 
 const app = new Hono<{
   Bindings: Env;
@@ -27,104 +13,8 @@ app.get("/health", (c) => {
   });
 });
 
-app.post(
-  "/auth/login/request-otp",
-  sValidator("json", loginRequestOtpSchema),
-  async (c): Promise<RawResponse> => {
-    const { email } = c.req.valid("json");
 
-    const db = createDb(c.env.DATABASE_URL);
 
-    const [user] = await db
-      .select({ userId: users.id, email: users.email })
-      .from(users)
-      .where(eq(users.email, email));
-
-    if (!user) {
-      return c.json(
-        {
-          success: true,
-          message: "OTP has been sent to user if exists",
-        },
-        200,
-      );
-    }
-
-    const [activeOtp] = await db
-      .select()
-      .from(verificationOtps)
-      .where(gt(verificationOtps.expiresAt, new Date()));
-
-    if (activeOtp) {
-      return c.json(
-        {
-          success: false,
-          message: "User already has an active OTP",
-        },
-        400,
-      );
-    }
-
-    const otp = generateOtp();
-
-    await db.insert(verificationOtps).values({
-      email,
-      otp,
-      expiresAt: new Date(Date.now() + 60 * 5 * 1000),
-    });
-
-    return c.json({
-      success: true,
-      message: "OTP has been sent to user if exists",
-    });
-  },
-);
-
-app.post(
-  "/auth/login/verify-otp",
-  sValidator("json", loginVerifyOtpSchema),
-  async (c): Promise<RawResponse<{ token: string }>> => {
-    const { email, otp } = c.req.valid("json");
-
-    const db = createDb(c.env.DATABASE_URL);
-
-    const [activeOtp] = await db
-      .select()
-      .from(verificationOtps)
-      .where(
-        and(eq(verificationOtps.otp, otp), eq(verificationOtps.email, email)),
-      )
-      .orderBy(desc(verificationOtps.createdAt))
-      .limit(1);
-
-    if (!activeOtp) {
-      return c.json(
-        {
-          success: false,
-          message: "Invalid OTP",
-        },
-        400,
-      );
-    } else if (activeOtp.expiresAt < new Date()) {
-      return c.json(
-        {
-          success: false,
-          message: "OTP has expired, please request a new one",
-        },
-        400,
-      );
-    }
-
-    const token = await getJWTSession({ email }, c.env.SECRET);
-
-    return c.json({
-      success: true,
-      message: "OTP has been verified successfully, proceed to log in",
-      data: {
-        token,
-      },
-    });
-  },
-);
+routes(app);
 
 export default app;
